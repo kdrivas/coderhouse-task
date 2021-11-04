85c72089d2d0bfb11a9624c073cd1fed76e78baf
kervyRivas <a20112128@pucp.pe>
2021-11-03 22:32:49 -0500

    adding task_06 part 1

---
+++ task_06/src/server.js
@@ -0,0 +1,43 @@
+const exphbs = require('express-handlebars');
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+const products = []
+
+io.on('connection', (socket) => { 
+  console.log('Usuario conectado') 
+
+  socketStored = socket
+
+  // Registrando socket para usarlo en routes
+  app.set('socketio', socket)
+  
+  socket.emit('products', products)
+  socket.on('update', product => {
+    products.push(product)
+    io.sockets.emit('products', products)
+  })
+})
+
+const routerProductos = require('./routes.js')
+
+app.use('/', express.static('public'))
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'index.hbs',
+}))
+app.set('views', './public/views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/api/productos', routerProductos)
+
+const port = process.env.PORT || 8080;
+const server = httpServer.listen(port, () => {
+  console.log(`connected to ${port}`)
+})
+
+server.on('error', error => console.log(error))
