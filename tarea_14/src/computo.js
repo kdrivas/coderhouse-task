721a5c53660e01957e9404e92fb7a9712f376da4
kervyRivas <a20112128@pucp.pe>
2022-01-15 12:04:44 -0500

    tarea_14

---
+++ tarea_14/src/computo.js
@@ -0,0 +1,32 @@
+const calculo = () => {
+  let d = {}
+  for (let i = 0; i < 1000; i++) {
+      if (i in d)
+        d[i] += 1
+      else
+        d[i] = 0
+  }
+  return d
+}
+
+process.on('exit', () => {
+  console.log(`worker #${process.pid} cerrado`)
+})
+
+// process.on('message', msg => {
+//   console.log(`worker #${process.pid} iniciando su tarea`)
+//   const d = 
+//   process.send(d)
+//   console.log(`worker #${process.pid} finalizó su trabajo`)
+//   process.exit()
+// })
+
+process.on('mensaje', (msj) => {
+  // hacer el calculo
+  onsole.log(`worker #${process.pid} iniciando su tarea`)
+  const result = calculo()
+  console.log(`worker #${process.pid} finalizó su trabajo`)
+  return { result }
+})
+
+process.send({ isReady: true })
